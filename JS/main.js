0f1d729cafe46c42f854267b4d75500241b8ef2c
Yogesh SA <yogesh.5prog@gmail.com>
2023-02-09 13:16:54 +0530

    first js

---
+++ JS/main.js
@@ -0,0 +1,55 @@
+function checkAnswer(){
+    const input = document.getElementById("input");
+    const status = document.getElementById("status");
+
+if(input.value=="charles"){
+   status.innerText = "correct";
+}
+else{
+    status.innerText="wrong";
+}
+}
+function checkAnswer2(){
+    const input2 = document.getElementById("input2");
+    const status2 = document.getElementById("status2");
+
+if(input2.value=="8"){
+   status2.innerText = "correct";
+}
+else{
+    status2.innerText="wrong";
+}
+}
+function checkAnswer3(){
+    const input3 = document.getElementById("input3");
+    const status3 = document.getElementById("status3");
+
+if(input3.value=="search engine"){
+   status3.innerText = "correct";
+}
+else{
+    status3.innerText="wrong";
+}
+}
+function checkAnswer4(){
+    const input4 = document.getElementById("input4");
+    const status4 = document.getElementById("status4");
+
+if(input4.value=="random access memory"){
+   status4.innerText = "correct";
+}
+else{
+    status4.innerText="wrong";
+}
+}
+function checkAnswer5(){
+    const input5 = document.getElementById("input5");
+    const status5 = document.getElementById("status5");
+
+if(input5.value=="read only memory"){
+   status5.innerText = "correct";
+}
+else{
+    status5.innerText="wrong";
+}
+}
